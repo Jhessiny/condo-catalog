25226d2fe4001c25bf97aa2f85ca52407b0be603
jhessiny <jhessinymattos@gmail.com>
2023-06-26 17:32:14 -0300

    chore: adjust test config to consider test folder in the root directory

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,8 @@ import type { Config } from 'jest'
 
 const config: Config = {
   verbose: true,
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/spec.ts'],
+  roots: ['<rootDir>/test'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/test/**/spec.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
